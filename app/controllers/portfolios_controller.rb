3766d7a760c5279578293c620b3cf07ec3f1b0a3
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-02 16:15:35 +0200

    Added new and create action for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,26 @@ class PortfoliosController < ApplicationController
   def index
     @portfilio_items = Portfolio.all
   end
+
+  def new
+    @portfilio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :index, status: :created, location: @portfolio }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
