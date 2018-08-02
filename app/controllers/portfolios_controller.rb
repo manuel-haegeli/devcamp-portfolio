223b2cbccea7029da47c69fd43e0249a7988f8a2
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-02 16:49:39 +0200

    Implemented edit and update function

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: %i[edit update]
+
   def index
     @portfilio_items = Portfolio.all
   end
@@ -19,6 +21,25 @@ class PortfoliosController < ApplicationController
         format.json { render json: @portfolio.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfilio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfilio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def set_portfolio
+    @portfilio_item = Portfolio.find(params[:id])
   end
 
   def portfolio_params
