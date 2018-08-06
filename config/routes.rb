21badd7df7f56e97bc85dffb47cf3dc6bff38103
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-06 13:37:00 +0200

    Customize portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: %i[show edit]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
+  get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
