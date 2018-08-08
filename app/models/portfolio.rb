10592d7698ab0edad75dc050977bf96233d7f910
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-08 16:05:45 +0200

    Implemented image defaults, use main image for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   friendly_id :title, use: :slugged
 
   validates_presence_of :title, :body, :main_image, :thumbnail_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumbnail_image ||= 'http://via.placeholder.com/350x200'
+  end
 end
