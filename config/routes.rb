00ebfa21057e525f01d74f3d7b3a94047d2d9580
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-11 13:37:27 +0200

    Change authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: %i[show edit]
   get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
   get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
