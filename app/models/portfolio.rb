93e56a0eaeda4183fc4b4a7dfb677f858c68a140
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-11 12:12:39 +0200

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,7 @@
 # A highlighted past project
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: ->(attributes) { attributes['name'].blank? }
   include Placeholder
   extend FriendlyId
   friendly_id :title, use: :slugged
@@ -14,5 +15,9 @@ class Portfolio < ApplicationRecord
   def set_defaults
     self.main_image ||= Placeholder.image(width: 600, height: 400)
     self.thumbnail_image ||= Placeholder.image(width: 350, height: 200)
+  end
+
+  def self.angular
+    where(subtitle: 'Angular')
   end
 end
