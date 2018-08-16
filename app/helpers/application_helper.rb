3896b5a3b1a3dc6b8466d360e7838e42d098f3f5
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-16 14:18:20 +0200

    Implement content tag helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,16 @@ module ApplicationHelper
   def login_helper
     if current_user.is_a?(User)
       (link_to 'Logout', destroy_user_session_path, method: :delete) +
-        '<br>'.html_safe +
+        content_tag(:br) +
         (link_to 'Userprofile', edit_user_registration_path)
     else
       (link_to 'Login', new_user_session_path) +
-        '<br>'.html_safe +
+        content_tag(:br) +
         (link_to 'Register', new_user_registration_path)
     end
+  end
+
+  def referral_helper
+    content_tag(:p, "Thanks for visiting me from #{session[:referral]}") if session[:referral]
   end
 end
