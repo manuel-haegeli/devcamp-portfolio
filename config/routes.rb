b0c6fd4bb997b73a331dbfad31c04cc850643754
Manuel Haegeli <manuel.haegeli@coupa.com>
2018-08-22 16:16:22 +0200

    Implemented topic views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  resources :topics, only: %i[index show]
   resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: %i[show edit] do
